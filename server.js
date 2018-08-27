b005780d0d74fce6f808fdf8b2075f38186032d0
Gustavo Canales <gustavo.canales@me.com>
2018-08-26 22:20:33 -0500

    Change mongoose connection

--- server.js
+++ server.js
@@ -9,12 +9,15 @@ var app = express();
 
 // Connect to the Mongo DB
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var databaseUri= "mongodb://localhost/mongoHeadlines";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+if (process.env.MONGODB_URI){
+  mongoose.connect(process.env.MONGODB_URI);
+} else{
+  mongoose.connect(databaseUri)
+}
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
